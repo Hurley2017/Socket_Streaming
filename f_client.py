bac43ebf9534fdeed81539415b48e9c72312e63d
Hurley2017 <luciefer9062hurley@gmail.com>
2023-09-09 13:46:25 +0530

    DGRAM failed!

--- f_client.py
+++ f_client.py
@@ -3,44 +3,45 @@ from Additional_Function  import Transfer, Reader
 from playsound import playsound
 from pydub import AudioSegment
 from pydub.playback import play
+import sys
+sys.path.append('/path/to/ffmpeg')
 
 Transfer = Transfer()
-Reader = Reader()  
+Reader = Reader()
 
-SERVER_ADDRESS = '127.0.0.1'
-SERVER_PORT = 40000
-ADDRESS = (SERVER_ADDRESS, SERVER_PORT)
+SERVER_ADDRESS = input("Enter Server Address : ")
+SERVER_PORT = 5000
 
-Client_Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+Client_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Client_Socket.connect((SERVER_ADDRESS, SERVER_PORT))
+Client_Socket.send("SnehoJoggo".encode())
 
-Client_Socket.sendto("SnehoJoggo".encode(), ADDRESS)
-
-Server_Message, Server_Address = Client_Socket.recvfrom(2048)
-Server_Message = Server_Message.decode()
-
-if Server_Message == "CCreq":
+Receipt = Client_Socket.recv(1024).decode()
+if Receipt == "CCReq":
+    print("Connection Rejected")
     Client_Socket.close()
+    exit()
 else:
-    Musics, Server_Address = Client_Socket.recvfrom(2048)
-    Musics = Musics.decode().split(",")
-    Reader.readFiles(Musics)
+    Files = Client_Socket.recv(2048).decode()
+    Files = Files.split(",")
+    Reader.readFiles(Files)
     while True:
         File_Input = int(input("Enter your choice : "))
-        if File_Input <= len(Musics) and File_Input > 0:
-            FileName = Musics[File_Input-1]
+        if File_Input <= len(Files):
+            FileName = Files[int(File_Input)-1]
             break
         else:
             Reader.readERROR("Invalid Input")
-    Client_Socket.sendto(FileName.encode(), ADDRESS)
-    FMTHeader, Server_Address = Client_Socket.recvfrom(44)
+    Client_Socket.send(FileName.encode())
+    FMTHeader = Client_Socket.recv(44)
     while True:
-        Chunk = Client_Socket.recvfrom(65536)[0]
-        if len(Chunk) == 0:
+        Hello = Client_Socket.recv(65536)
+        if len(Hello) == 0:
             break
         else:
-            print("got")
-            Chunk = Transfer.combineByte([FMTHeader, Chunk])
-            soundChunk = AudioSegment(Chunk, format='mp3')
+            Hello = Transfer.combineByte([FMTHeader, Hello])
+            soundChunk = AudioSegment(Hello, format='wav')
             play(soundChunk)
     Client_Socket.close()
-        
+
+
--- f_server.py
+++ f_server.py
@@ -1,39 +1,50 @@
 import socket
 import threading
 from Additional_Function import Transfer, Reader
-SERVER_ADDRESS = '127.0.0.1' 
-SERVER_PORT = 40000
 
 Transfer = Transfer()
 Reader = Reader()
 
-Server_Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
+SERVER_PORT = 5000
+
+Server_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 Server_Socket.bind((SERVER_ADDRESS, SERVER_PORT))
+Server_Socket.listen(1)
+print("Server is ready and listening for Clients...")
 
-
-def Process(Client_Address, Client_Port):
-    Client_Address = (Client_Address, Client_Port)
-    Musics = Transfer.listFile("Music")
-    Server_Socket.sendto(Musics.encode(), Client_Address)
-    FileName, Client_Address = Server_Socket.recvfrom(2048)
-    FileName = FileName.decode()
-    print(FileName)
-    FileByte = Transfer.returnByte(FileName, "Music")
-    Chunks = Transfer.splitByte(FileByte, 65536)
-    for Chunk in Chunks:
-        Server_Socket.sendto(Chunk, Client_Address)
-        break
-    for Chunk in Chunks[1:]:
-        print("Sending : " + FileName + " to " + str(Client_Address) + " ..." )
-        Server_Socket.sendto(Chunk, Client_Address)
-while True:
-    Client_Message, Client_Address = Server_Socket.recvfrom(2048)
-    print(Client_Address)
-    Client_Message = Client_Message.decode()
-    if Client_Message == "SnehoJoggo":
-        Server_Socket.sendto("Hello".encode(), Client_Address)
-        threading.Thread(target=Process, args=(Client_Address[0], Client_Address[1])).start()
+def Process(Client_Socket, Client_Address):
+    print("Connection Established with " + str(Client_Address))
+    Greet = Client_Socket.recv(1024).decode()
+    if Greet == "SnehoJoggo":
+        Client_Socket.send("Hello from Server".encode())
+        Files = Transfer.listFile('Music')
+        Client_Socket.send(Files.encode())
+        FileName = Client_Socket.recv(1024).decode()
+        FileName = Transfer.any2WAV(FileName, "Music")
+        FileSize = Transfer.fileSizeinBytes(FileName, "Music")
+        ByteFile = Transfer.returnByte(FileName, "Music")
+        Chunks = Transfer.splitByte(ByteFile, 65536)
+        print("Sending : " + FileName + " to " + str(Client_Address) + " ...")
+        for Chunk in Chunks:
+            Client_Socket.send(Chunk)
+        Client_Socket.send("EOF".encode())
+        print(str(FileSize) + " bytes sent")
+        try:
+            pass
+        except:
+            print("Failed to send...")        
     else:
-        Server_Socket.sendto("CCReq".encode(), Client_Address)
-        
+        Client_Socket.send("CCReq".encode())
+
+
+while True:
+    Client_Socket, Client_Address = Server_Socket.accept()
+    threading.Thread(target=Process, args=(Client_Socket, Client_Address)).start()
+Server_Socket.close()
+
+
+
+
+
 
