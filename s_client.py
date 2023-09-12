47b6046d31a92fa4f469ab54c38de460ab4ef0ef
Hurley2017 <luciefer9062hurley@gmail.com>
2023-09-13 02:39:30 +0530

    File Transfer Bug Fixed@

--- s_client.py
+++ s_client.py
@@ -1,17 +1,19 @@
 import socket
 from Additional_Function import Transfer, Reader
+from time import sleep
 
 Transfer = Transfer()
 Reader = Reader()
 
 SERVER_ADDRESS = input("Enter Server Address : ")
 SERVER_PORT = 5000
+PACKAGE_BYTE_LENGTH = 4096
 
 Client_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 Client_Socket.connect((SERVER_ADDRESS, SERVER_PORT))
 Client_Socket.send("SnehoJoggo".encode())
 
-Receipt = Client_Socket.recv(1024).decode()
+Receipt = Client_Socket.recv(PACKAGE_BYTE_LENGTH).decode()
 
 if Receipt == "CCReq":
     print("Connection Rejected")
@@ -22,27 +24,36 @@ else:
     User_Response = input("Enter your choice : ")
     if User_Response == "1":
         Client_Socket.send("Download".encode())
-        Files = Client_Socket.recv(2048).decode()
-        Files = Files.split(",")
+        Files = Client_Socket.recv(PACKAGE_BYTE_LENGTH).decode()
+        Files = Files.split(',')
         Reader.readFiles(Files)
         while True:
             File_Input = int(input("Enter your choice : "))
-            if File_Input <= len(Files):
+            if File_Input <= len(Files) and File_Input > 0:
                 FileName = Files[int(File_Input)-1]
                 break
             else:
                 Reader.readERROR("Invalid Input")
         Client_Socket.send(FileName.encode())
-        Chunks = []
-        while True:
-            Hello = Client_Socket.recv(65536)
-            if len(Hello) == 0:
-                break
-            Chunks.append(Hello)
-            print("Received: " + str(len(Hello)) + " bytes")
-        ByteFile = Transfer.combineByte(Chunks)
-        Transfer.saveByte(ByteFile, FileName, "Client")
-        Client_Socket.close()
+        try:
+            Package_Information = Client_Socket.recv(PACKAGE_BYTE_LENGTH).decode()
+            Package_Information = Package_Information.split(':')
+            Chunks = []
+            while True:
+                if len(Chunks) == int(Package_Information[0])-1:
+                    Chunk = Client_Socket.recv(int(Package_Information[1]))
+                    Chunks.append(Chunk)
+                    break
+                Chunk = Client_Socket.recv(PACKAGE_BYTE_LENGTH) 
+                Chunks.append(Chunk)
+            ByteFile = Transfer.combineByte(Chunks)
+            Transfer.saveByte(ByteFile, FileName, "Client")
+            FileSize = Transfer.fileSizeinBytes(FileName, "Client")
+            print("Received : " + str(FileSize) + " bytes successfully.")
+            Client_Socket.close()
+        except:
+            print("Failed to Receive...")
+            Client_Socket.close()
     elif User_Response == '2':
         Client_Socket.send("Upload".encode())
         Files = Transfer.listFile("Client")
@@ -56,14 +67,21 @@ else:
             else:
                 Reader.readERROR("Invalid Input")
         Client_Socket.send(FileName.encode())
-        FileSize = Transfer.fileSizeinBytes(FileName, "Client")
-        ByteFile = Transfer.returnByte(FileName, "Client")
-        Chunks = Transfer.splitByte(ByteFile, 65536)
-        for Chunk in Chunks:
-            Client_Socket.send(Chunk)
-            print("Sent: " + str(len(Chunk)) + " bytes")
-        Client_Socket.send("EOF".encode())
-        Client_Socket.close()
+        try:
+            FileSize = Transfer.fileSizeinBytes(FileName, "Client")
+            ByteFile = Transfer.returnByte(FileName, "Client")
+            Chunks = Transfer.splitByte(ByteFile, PACKAGE_BYTE_LENGTH)
+            Package_Information = str(len(Chunks)) + ',' + str(len(Chunks[-1]))
+            Package_Information = Package_Information.encode()
+            Client_Socket.send(Package_Information)
+            sleep(1)
+            for Chunk in Chunks:
+                Client_Socket.send(Chunk)
+            print("Sent : " + str(FileSize) + " bytes successfully.")
+            Client_Socket.close()
+        except:
+            print("Failed to Send...")
+            Client_Socket.close()
     else:
         Client_Socket.close()
 
--- s_server.py
+++ s_server.py
@@ -1,12 +1,14 @@
 import socket
 import threading
 from Additional_Function import Transfer, Reader
+from time import sleep
 
 Transfer = Transfer()
 Reader = Reader()
 
 SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
 SERVER_PORT = 5000
+PACKAGE_BYTE_LENGTH = 4096
 
 Server_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 Server_Socket.bind((SERVER_ADDRESS, SERVER_PORT))
@@ -15,45 +17,47 @@ print("Server is ready and listening for Clients...")
 
 def Process(Client_Socket, Client_Address):
     print("Connection Established with " + str(Client_Address))
-    Greet = Client_Socket.recv(1024).decode()
+    Greet = Client_Socket.recv(PACKAGE_BYTE_LENGTH).decode()
     if Greet == "SnehoJoggo":
         Client_Socket.send("Hello from Server".encode())
-        Receipt = Client_Socket.recv(1024).decode()
+        Receipt = Client_Socket.recv(PACKAGE_BYTE_LENGTH).decode()
         if Receipt == "Download":
             try:
-                Files = Transfer.listFile('Server')
-                Client_Socket.send(Files.encode())
-                FileName = Client_Socket.recv(1024).decode()
+                Files = Transfer.listFile("Server").encode()
+                Client_Socket.send(Files)
+                FileName = Client_Socket.recv(PACKAGE_BYTE_LENGTH).decode()
                 FileSize = Transfer.fileSizeinBytes(FileName, "Server")
                 ByteFile = Transfer.returnByte(FileName, "Server")
-                Chunks = Transfer.splitByte(ByteFile, 65536)
-                print("Sending : " + FileName + " to " + str(Client_Address) + " ...")
+                Chunks = Transfer.splitByte(ByteFile, PACKAGE_BYTE_LENGTH)
+                Package_Information = str(len(Chunks)) + ":" + str(len(Chunks[-1]))
+                Package_Information = Package_Information.encode()
+                Client_Socket.send(Package_Information)
+                sleep(1)
                 for Chunk in Chunks:
                     Client_Socket.send(Chunk)
-                Client_Socket.send("EOF".encode())
-                print(str(FileSize) + " bytes sent")
+                print("Sent : " + str(FileSize) + " bytes successfully to " + str(Client_Address) + ".")
             except:
-                print("Failed to send...")
+                print("Failed to Send...")
         elif Receipt == "Upload":
-            FileName = Client_Socket.recv(1024).decode()
-            Chunks = []
-            print("Receiving : " + FileName + " from " + str(Client_Address) + " ...")
             try:
+                FileName = Client_Socket.recv(PACKAGE_BYTE_LENGTH).decode()
+                Package_Information = Client_Socket.recv(PACKAGE_BYTE_LENGTH).decode()
+                Package_Information = Package_Information.split(',')
+                Chunks = []
                 while True:
-                    Hello = Client_Socket.recv(65536)
-                    if len(Hello) == 0:
+                    if len(Chunks) == int(Package_Information[0])-1:
+                        Chunk = Client_Socket.recv(int(Package_Information[1]))
+                        Chunks.append(Chunk)
                         break
-                    Chunks.append(Hello)
+                    Chunk = Client_Socket.recv(PACKAGE_BYTE_LENGTH) 
+                    Chunks.append(Chunk)
                 ByteFile = Transfer.combineByte(Chunks)
                 Transfer.saveByte(ByteFile, FileName, "Server")
-                FileSize = Transfer.fileSizeinBytes(FileName, "Server")
-                print(str(FileSize) + " bytes received")
+                print("Received : " + str(FileSize) + " bytes successfully from " + str(Client_Address) + ".")
             except:
                 print("Failed to Receive...")
-            
     else:
         Client_Socket.send("CCReq".encode())
-
 
 while True:
     Client_Socket, Client_Address = Server_Socket.accept()
